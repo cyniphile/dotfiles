47bf7a715121201ec838dee248b3063fea5474fc
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2022-12-12 12:18:27 -0500

    update

--- switch_ctrl_alt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mv /usr/share/X11/xkb/symbols/pc /usr/share/X11/xkb/symbols/pc.bak
-mv /usr/share/X11/xkb/symbols/altwin /usr/share/X11/xkb/symbols/altwin.bak
-cp ~/dotfiles/usr-share-X11-xkb-symbols/pc /usr/share/X11/xkb/symbols/pc
-cp ~/dotfiles/usr-share-X11-xkb-symbols/altwin /usr/share/X11/xkb/symbols/altwin
-echo "please reboot to effect changes"
