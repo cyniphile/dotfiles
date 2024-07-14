ce6d93d78363dc0e76b5a06c5936dcf6f4d134b5
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2024-07-14 17:26:56 -0400

    add newline back

--- .p10k.zsh
+++ .p10k.zsh
@@ -56,6 +56,7 @@
      #command_execution_time  # previous command duration
      virtualenv              # python virtual environment
     # =========================[ Line #2 ]=========================
+    newline #\n
     prompt_char # prompt symbol
   )
 
